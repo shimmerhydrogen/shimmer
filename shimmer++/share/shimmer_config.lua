d55c087465aeb1a8fe0c5bcb2d883ae8de6320a2
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2023-07-20 12:19:05 +0200

    Lua config file.

---
+++ shimmer++/share/shimmer_config.lua
@@ -0,0 +1,4 @@
+config.xlsx_input_file = "../../matlab/Mod_SHIMM_v01/INPUT_Pambour1.xlsx";
+config.node_worksheet = "PIPE";
+
+
--- shimmer++/src/main.cpp
+++ shimmer++/src/main.cpp
@@ -6,17 +6,36 @@
 #include <QWidget>
 
 #include "OpenXLSX.hpp"
+#include "sol/sol.hpp"
 
 using namespace OpenXLSX;
+
+bool init_lua(sol::state& lua)
+{
+    lua.open_libraries(sol::lib::base);
+    lua.create_table("config");
+    sol::load_result script = lua.load_file("../share/shimmer_config.lua");
+    if (!script.valid()) {
+        std::cout << "Can't load config file" << std::endl;
+        return false;
+    }
+    script();
+    return true;
+}
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
 
+    sol::state lua;
+    if (not init_lua(lua))
+        return 1;
+
     XLDocument doc;
-    std::string path = "../../matlab/Mod_SHIMM_v01/INPUT_Pambour1.xlsx";
+    std::string path = lua["config"]["xlsx_input_file"];
     doc.open(path);
-    auto wks = doc.workbook().worksheet("PIPE");
+    std::string worksheet_name = lua["config"]["node_worksheet"];
+    auto wks = doc.workbook().worksheet(worksheet_name);
 
     size_t nrows = 0;
     for (auto& row : wks.rows())
@@ -25,6 +44,7 @@ int main(int argc, char **argv)
     }
 
     std::cout << nrows << std::endl;
+
 
     QWidget w;
     w.show();

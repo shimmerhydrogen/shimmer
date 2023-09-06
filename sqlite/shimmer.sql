548fc5184cb3cfc9bf549d389746be47a1c1ba44
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2023-09-06 18:37:15 +0200

    sql

--- sqlite/shimmer.sql
+++ sqlite/shimmer.sql
@@ -1,67 +1,85 @@
-create table nodes (
-    n_name      TEXT,
-    n_number    INTEGER,
-    n_height    REAL,
-    PRIMARY KEY(n_number)
+-- The stations. They are the nodes of the graph
+create table stations (
+    s_name      TEXT,
+    s_number    INTEGER,
+    s_height    REAL,
+    PRIMARY KEY(s_number)
 );
 
-create table edge_types (
-    e_type      INTEGER,
-    type_name   TEXT NOT NULL,
-    PRIMARY KEY (e_type)
+-- Pipeline element type. Can be a pipe, a compressor, a regulator, ...
+create table pipeline_types (
+    p_type      INTEGER,
+    t_name      TEXT NOT NULL,
+    PRIMARY KEY (p_type)
 );
 
-create table edges (
-    e_name      TEXT NOT NULL,
-    n_from      INTEGER,
-    n_to        INTEGER,    
-    e_type      INTEGER,
-    PRIMARY KEY (e_name, n_from, n_to),
-    FOREIGN KEY (n_from)
-        REFERENCES nodes(n_number),
-    FOREIGN KEY (n_to)
-        REFERENCES nodes(n_number)
-    FOREIGN KEY (e_type)
-        REFERENCES edge_types(e_type)
+-- The pipelines. They are the edges of the graph.
+create table pipelines (
+    p_name      TEXT NOT NULL,
+    s_from      INTEGER,
+    s_to        INTEGER,    
+    p_type      INTEGER,
+    PRIMARY KEY (p_name, s_from, s_to),
+
+    -- The source station must exist
+    FOREIGN KEY (s_from)
+        REFERENCES stations(s_number),
+    -- The destination station must exist
+    FOREIGN KEY (s_to)
+        REFERENCES stations(s_number)
+    -- The pipeline type must be valid
+    FOREIGN KEY (p_type)
+        REFERENCES pipeline_types(p_type)
 );
 
+-- Pipeline parameters as length, diameter and so on.
 create table pipeline_parameters (
-    e_name      TEXT_NOT_NULL,
-    n_from      INTEGER,
-    n_to        INTEGER,
+    p_name      TEXT_NOT_NULL,
+    s_from      INTEGER,
+    s_to        INTEGER,
     length      REAL NOT NULL,
     diameter    REAL NOT NULL,
     epsi        REAL NOT NULL,
-    FOREIGN KEY (e_name, n_from, n_to)
-        REFERENCES edges(e_name, n_from, n_to)
+
+    -- The referenced pipeline must exist
+    FOREIGN KEY (p_name, s_from, s_to)
+        REFERENCES pipelines(p_name, s_from, s_to)
 );
 
+-- The gases. Which are the parameters associated to each gas?
 create table gases (
     g_name      TEXT UNIQUE NOT NULL
 );
 
+-- Who injects what
 create table injects (
     s_number    INTEGER,
     g_name      TEXT,
     quantity    REAL,
+
+    -- station number must be valid
     FOREIGN KEY (s_number)
         REFERENCES stations(s_number),
+    -- gas name must be valid
     FOREIGN KEY (g_name)
         REFERENCES gases(g_name)
 );
 
-insert into edge_types values (0, 'pipeline'), (1, 'resistor'),
-    (2, 'compressor'), (3, 'regulator'), (4, 'valve');
+insert into pipeline_types values (0, 'pipeline');
+insert into pipeline_types values (1, 'resistor');
+insert into pipeline_types values (2, 'compressor');
+insert into pipeline_types values (3, 'regulator');
+insert into pipeline_types values (4, 'valve');
 
-insert into nodes values ('station1', 1, 0);
-insert into nodes values ('station2', 2, 0);
-insert into nodes values ('station3', 3, 0);
-insert into nodes values ('station4', 4, 0);
-insert into nodes values ('station5', 5, 0);
+insert into stations values ('station1', 1, 0);
+insert into stations values ('station2', 2, 0);
+insert into stations values ('station3', 3, 0);
+insert into stations values ('station4', 4, 0);
+insert into stations values ('station5', 5, 0);
 
-insert into edges values ('pipe1', 1, 2, 0);
-insert into edges values ('pipe2', 1, 3, 0);
-insert into edges values ('pipe3', 2, 4, 0);
+insert into pipelines values ('pipe1', 1, 2, 0);
+insert into pipelines values ('pipe2', 1, 3, 0);
+insert into pipelines values ('pipe3', 2, 4, 0);
 
 insert into pipeline_parameters values ('pipe1', 1, 2, 100, 10, 0);
 insert into pipeline_parameters values ('pipe2', 1, 3, 200, 5, 0);
@@ -75,11 +93,13 @@ insert into injects values (2, 'gas1', 0.3);
 insert into injects values (2, 'gas2', 0.3); 
 insert into injects values (2, 'gas3', 0.3); 
 
-#select stations.s_name, injects.g_name, injects.quantity
-#    from stations inner join injects on stations.s_number = injects.s_number
-#    where stations.s_number = 2;
+-- Ask which gases are injected by station 2
+select stations.s_name, injects.g_name, injects.quantity
+    from stations inner join injects on stations.s_number = injects.s_number
+    where stations.s_number = 2;
 
-#select stations.s_name, sum(injects.quantity)
-#    from stations inner join injects on stations.s_number = injects.s_number
-#    group by stations.s_name;
+-- Ask the total quantity of gas injected by each station
+select stations.s_name, sum(injects.quantity)
+    from stations inner join injects on stations.s_number = injects.s_number
+    group by stations.s_name;
 

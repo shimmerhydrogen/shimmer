5ba05d8a80511edaec2d786743e7ef5d4576edd1
fabio.vicini <fabio.vicini@polito.it>
2023-12-28 11:16:17 +0100

    starting add functions

---
+++ GERG/src/GERG_functions.hpp
@@ -0,0 +1,73 @@
+#ifndef __GERG_functions_H
+#define __GERG_functions_H
+
+namespace GERG
+{
+  // iFlag=0;
+  // [Tr_b,Dr_b] = ReducingParametersGERG(Aplus'*reshape(CC_gas(:,:,1),dimn,21));
+  // [Tcx_b,Dcx_b,Vcx_b]=PseudoCriticalPointGERG(Aplus'*reshape(CC_gas(:,:,1),dimn,21),dimb);
+  // [Pcheck, Zm, Den]=PropertiesGERG(iFlag, pm(:,1)/1e3, Tb, Aplus'*reshape(CC_gas(:,:,1),dimn,21),dimb,Tr_b,Dr_b,Tcx_b,Dcx_b,Vcx_b);
+
+  template <class matrix_type>
+  struct Reducing_parameters final
+  {
+      matrix_type Tr; ///< reducing temperature (K)
+      matrix_type Dr; ///< reducing density (mol/l)
+  };
+
+  template <class matrix_type>
+  struct Pseudo_critical_point final
+  {
+      matrix_type Tcx; ///< pseudo critical point temperature (K)
+      matrix_type Dcx; ///< pseudo critical point density (mol/l)
+      matrix_type Vcx; ///< pseudo critical point velocity (?)
+  };
+
+  template <class matrix_type>
+  struct Thermodynamic_properties final
+  {
+      matrix_type P1; ///< a pressure (?)
+      matrix_type Z; ///< compressibility factor
+      matrix_type D; ///< density (mol/l)
+      matrix_type gamma; ///< a constant (?)
+  };
+
+  /// \brief Calculate reducing variables.
+  /// \param x, composition (mole fraction)
+  /// \return the reducing parameters struct
+  /// \note Only need to call this if the composition has changed.
+  template <class matrix_type>
+  Reducing_parameters<matrix_type> reducing_parameters(const matrix_type& x);
+  /// \brief Calculate a pseudo critical point
+  /// as the mole fraction average
+  /// of the critical temperatures and critical volumes
+  /// \param x, composition (mole fraction)
+  /// \param dimn, the composition size
+  /// \return the pseudo critical point struct
+  template <class matrix_type>
+  Pseudo_critical_point<matrix_type> pseudo_critical_point(const matrix_type& x,
+                                                           const typename matrix_type::size_type& dimn);
+  /// \brief Calculate thermodynamic properties as a function of temperature and density.
+  /// \param P, pressure (kPa), size dimn x 1
+  /// \param T, temperature (K), size dimn x 1
+  /// \param x, composition (mole fraction)
+  /// \param dimn, the composition size
+  /// \param Tr, Dr, the reducing temperature (K) and the reducing density (mol/l), size dimn x 1
+  /// \param Tcx, Dcx, Vcx, the pseudo critical point temperature, density and velocity, size dimn x 1
+  /// \param parameters, other input parameters
+  /// \return the thermodynamic properties struct
+  template <class matrix_type,
+            class parameters_type>
+  Thermodynamic_properties<matrix_type> thermodynamic_properties(const matrix_type& P,
+                                                                 const matrix_type& T,
+                                                                 const matrix_type& x,
+                                                                 const typename matrix_type::size_type& dimn,
+                                                                 const matrix_type& Tr,
+                                                                 const matrix_type& Dr,
+                                                                 const matrix_type& Tcx,
+                                                                 const matrix_type& Dcx,
+                                                                 const matrix_type& Vcx,
+                                                                 const parameters_type& parameters);
+}
+
+#endif // __GERG_functions_H
---
+++ GERG/src/MATLAB_GERG_functions.hpp
@@ -0,0 +1,26 @@
+#ifndef __MATLAB_GERG_functions_H
+#define __MATLAB_GERG_functions_H
+
+#include "GERG_functions.hpp"
+
+namespace GERG
+{
+  // iFlag=0;
+  // [Tr_b,Dr_b] = ReducingParametersGERG(Aplus'*reshape(CC_gas(:,:,1),dimn,21));
+  // [Tcx_b,Dcx_b,Vcx_b]=PseudoCriticalPointGERG(Aplus'*reshape(CC_gas(:,:,1),dimn,21),dimb);
+  // [Pcheck, Zm, Den]=PropertiesGERG(iFlag, pm(:,1)/1e3, Tb, Aplus'*reshape(CC_gas(:,:,1),dimn,21),dimb,Tr_b,Dr_b,Tcx_b,Dcx_b,Vcx_b);
+
+  struct Thermodynamic_properties_parameters final
+  {
+      enum struct Types
+      {
+        Gas_phase = 0, // strict pressure solver in gas phase without checks (fastest mode, but output state may not be stable single phase)
+        Two_phase = 1, // to make checks for possible 2-phase state (result may still not be stable single phase, but many unstable states will be identified)
+        Liquid_phase = 2// to search for liquid phase (and make the same checks when iFlag=1)
+      };
+
+      Types Type;
+  };
+}
+
+#endif // __MATLAB_GERG_functions_H

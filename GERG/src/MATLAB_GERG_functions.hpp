9cdac1da5f048bbeacc71751f522f13915643232
fabio.vicini <fabio.vicini@polito.it>
2023-12-29 11:15:11 +0100

    fix

--- GERG/src/MATLAB_GERG_functions.hpp
+++ GERG/src/MATLAB_GERG_functions.hpp
@@ -11,9 +11,9 @@ namespace GERG
   {
       enum struct Types
       {
-        Gas_phase = 0, // strict pressure solver in gas phase without checks (fastest mode, but output state may not be stable single phase)
-        Two_phase = 1, // to make checks for possible 2-phase state (result may still not be stable single phase, but many unstable states will be identified)
-        Liquid_phase = 2// to search for liquid phase (and make the same checks when iFlag=1)
+        Gas_phase = 0, ///< strict pressure solver in gas phase without checks (fastest mode, but output state may not be stable single phase)
+        Two_phase = 1, ///< to make checks for possible 2-phase state (result may still not be stable single phase, but many unstable states will be identified)
+        Liquid_phase = 2 ///< to search for liquid phase (and make the same checks when iFlag=1)
       };
 
       Types Type;

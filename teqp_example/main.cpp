12058c744c548b278ce7407ff3e8edaf28bb655e
fabio.vicini <fabio.vicini@polito.it>
2024-07-31 17:52:39 +0200

    still fixing

--- teqp_example/main.cpp
+++ teqp_example/main.cpp
@@ -8,30 +8,11 @@ int main(int argc, char **argv)
 	using namespace teqp::cppinterface;
 	using namespace teqp::cppinterface::adapter;
 
-	//# Note that names are case-sensitive; this doesn't work
-	//model = teqp.make_model({'kind':"GERG2008resid", 'model':{"names": ['methane','ethane']}})
-
-	//auto j = R"(
-	//{"kind": "GERG2008resid", "model": {"names": ['methane','ethane']}}
-	//)"_json;
-	
-	/*nlohmann::json j = { 
-        {"kind", "GERG2008resid"}, 
-        {"model", {"names", ["methane","ethane"]}}
-    };
-    std::cout << j.dump(2);
-	auto ptr = make_model(j);*/
-	
-	nlohmann::json j = { 
-        {"kind", "multifluid"}, 
-        {"model", {
-            {"components", {"../mycp/dev/fluids/Methane.json","../mycp/dev/fluids/Ethane.json"}},
-            {"BIP", "../mycp/dev/mixtures/mixture_binary_pairs.json"},
-            {"departure", "../mycp/dev/mixtures/mixture_departure_functions.json"}
-        }
-    }};
-    //std::cout << j.dump(2);
-    auto am = teqp::cppinterface::make_model(j);
+	auto j = R"(
+	{"kind": "GERG2008resid", "model": {"names": ["methane","ethane"]}}
+	)"_json;
+	std::cout<< "j: " << j.dump(2)<< std::endl;
+	auto ptr = make_model(j);
 
 	return 0;
 }

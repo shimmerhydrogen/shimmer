119f386161b7c7b7aacde0b2f6c1665ef496ad02
fabio.vicini <fabio.vicini@polito.it>
2024-07-31 17:40:23 +0200

    still fixing

--- teqp_example/main.cpp
+++ teqp_example/main.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include "teqp/cpp/teqpcpp.hpp"
 #include "teqp/cpp/deriv_adapter.hpp"
+#include "teqp/cpp/derivs.hpp"
 
 int main(int argc, char **argv) 
 {
@@ -9,10 +11,27 @@ int main(int argc, char **argv)
 	//# Note that names are case-sensitive; this doesn't work
 	//model = teqp.make_model({'kind':"GERG2008resid", 'model':{"names": ['methane','ethane']}})
 
-	auto j = R"(
-	{"kind": "GERG2008resid", "model": {"names": ['methane','ethane']}}
-	)"_json;
-	auto ptr = make_model(j);
+	//auto j = R"(
+	//{"kind": "GERG2008resid", "model": {"names": ['methane','ethane']}}
+	//)"_json;
+	
+	/*nlohmann::json j = { 
+        {"kind", "GERG2008resid"}, 
+        {"model", {"names", ["methane","ethane"]}}
+    };
+    std::cout << j.dump(2);
+	auto ptr = make_model(j);*/
+	
+	nlohmann::json j = { 
+        {"kind", "multifluid"}, 
+        {"model", {
+            {"components", {"../mycp/dev/fluids/Methane.json","../mycp/dev/fluids/Ethane.json"}},
+            {"BIP", "../mycp/dev/mixtures/mixture_binary_pairs.json"},
+            {"departure", "../mycp/dev/mixtures/mixture_departure_functions.json"}
+        }
+    }};
+    //std::cout << j.dump(2);
+    auto am = teqp::cppinterface::make_model(j);
 
 	return 0;
 }

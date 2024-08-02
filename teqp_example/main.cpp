79539e0b5d6171125d91d5b12923324b3502aa26
fabio.vicini <fabio.vicini@polito.it>
2024-08-02 15:19:31 +0200

    add: some test general

--- teqp_example/main.cpp
+++ teqp_example/main.cpp
@@ -3,16 +3,28 @@
 #include "teqp/cpp/deriv_adapter.hpp"
 #include "teqp/cpp/derivs.hpp"
 
+#include <typeinfo>
+
 int main(int argc, char **argv) 
 {
-	using namespace teqp::cppinterface;
-	using namespace teqp::cppinterface::adapter;
+  using namespace teqp::cppinterface;
+  using namespace teqp::cppinterface::adapter;
 
-	auto j = R"(
+  auto j = R"(
 	{"kind": "GERG2008resid", "model": {"names": ["methane","ethane"]}}
 	)"_json;
-	std::cout<< "j: " << j.dump(2)<< std::endl;
-	auto ptr = make_model(j);
+  std::cout<< "model GERG: " << j.dump(2)<< std::endl;
 
-	return 0;
+  auto model = make_model(j);
+
+  const auto molefrac = (Eigen::ArrayXd(2) << 0.2, 0.8).finished();
+  const double T = 300;
+  const double rho_molar = 3;
+
+  const auto R = model->R(molefrac);
+  std::cout<< "R: " << R << std::endl;
+  const auto Ar_01 = model->get_Arxy(0, 1, T, rho_molar, molefrac);
+  std::cout<< "Ar_01: " << Ar_01 << std::endl;
+
+  return 0;
 }

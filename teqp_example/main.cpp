5643df4b8b4df232dbca3e8ee80d3ce3559a7b64
fabio.vicini <fabio.vicini@polito.it>
2024-08-07 16:53:52 +0200

    add new properties

--- teqp_example/main.cpp
+++ teqp_example/main.cpp
@@ -2,6 +2,7 @@
 #include "teqp/cpp/teqpcpp.hpp"
 #include "teqp/cpp/deriv_adapter.hpp"
 #include "teqp/cpp/derivs.hpp"
+#include "teqp/models/GERG/GERG.hpp"
 
 #include <typeinfo>
 
@@ -10,21 +11,25 @@ int main(int argc, char **argv)
   using namespace teqp::cppinterface;
   using namespace teqp::cppinterface::adapter;
 
-  auto j = R"(
-	{"kind": "GERG2008resid", "model": {"names": ["methane","ethane"]}}
-	)"_json;
-  std::cout<< "model GERG: " << j.dump(2)<< std::endl;
+  // auto j = R"(
+  // {"kind": "GERG2008resid", "model": {"names": ["methane","ethane"]}}
+  // )"_json;
+  // std::cout<< "model GERG: " << j.dump(2)<< std::endl;
 
-  auto model = make_model(j);
+  //auto model = make_model(j);
+
+  const auto& names = teqp::GERG2008::component_names;
+  std::vector<std::string> comps = {"carbondioxide", "methane"};
+  auto model = teqp::GERG2008::GERG2008ResidualModel(comps);
 
   const auto molefrac = (Eigen::ArrayXd(2) << 0.2, 0.8).finished();
   const double T = 300;
   const double rho_molar = 3;
 
-  const auto R = model->R(molefrac);
+  const auto R = model.R(molefrac);
   std::cout<< "R: " << R << std::endl;
-  const auto Ar_01 = model->get_Arxy(0, 1, T, rho_molar, molefrac);
-  std::cout<< "Ar_01: " << Ar_01 << std::endl;
+  const auto Tc = model.red.get_Tcvec();
+  const auto Vc = model.red.get_vcvec();
 
   return 0;
 }

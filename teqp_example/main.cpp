d1d66dd76130bd291295694eece62582d96a4138
fabio.vicini <fabio.vicini@polito.it>
2024-08-14 17:48:35 +0200

    still fixing

--- teqp_example/main.cpp
+++ teqp_example/main.cpp
@@ -4,34 +4,40 @@
 #include "teqp/cpp/derivs.hpp"
 #include "teqp/models/GERG/GERG.hpp"
 
-#include <typeinfo>
+#include "shimmer_teqp_utilities.hpp"
 
 int main(int argc, char **argv) 
 {
   using namespace teqp::cppinterface;
   using namespace teqp::cppinterface::adapter;
 
-  // auto j = R"(
-  // {"kind": "GERG2008resid", "model": {"names": ["methane","ethane"]}}
-  // )"_json;
-  // std::cout<< "model GERG: " << j.dump(2)<< std::endl;
+  constexpr double tolerance = std::numeric_limits<double>::epsilon();
+  Eigen::ArrayXd x(21);
+  x<< 0.8, 0, 0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
 
-  //auto model = make_model(j);
+  const auto filter = shimmer_teqp_utilities::filter_components(x,
+                                                                tolerance);
 
   const auto& names = teqp::GERG2008::component_names;
-  std::vector<std::string> comps = {"carbondioxide", "methane"};
+  const auto comps = shimmer_teqp_utilities::filter_collection(filter,
+                                                               names);
+  const auto mol_frac = shimmer_teqp_utilities::filter_collection(filter,
+                                                                  x);
+
+  std::cout<< "Comps: "<< comps<< std::endl;
+  std::cout<< "Mol_frac: "<< mol_frac<< std::endl;
+
   auto model = teqp::GERG2008::GERG2008ResidualModel(comps);
 
-  const auto molefrac = (Eigen::ArrayXd(2) << 0.2, 0.8).finished();
   const double T = 300;
   const double rho_molar = 3;
 
-  const auto R = model.R(molefrac);
+  const auto R = model.R(mol_frac);
   std::cout<< "R: " << R << std::endl;
   const auto Tc = model.red.get_Tcvec();
   const auto Vc = model.red.get_vcvec();
-  const auto Tr = model.red.get_Tr(molefrac);
-  const auto rho_r = model.red.get_rhor(molefrac);
+  const auto Tr = model.red.get_Tr(mol_frac);
+  const auto rho_r = model.red.get_rhor(mol_frac);
 
   return 0;
 }

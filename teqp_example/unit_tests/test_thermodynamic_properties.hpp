4410b7fc0573bd4ae978d084b9b72bb77fa1b897
fabio.vicini <fabio.vicini@polito.it>
2024-11-06 11:24:27 +0100

    still fixing

--- teqp_example/unit_tests/test_thermodynamic_properties.hpp
+++ teqp_example/unit_tests/test_thermodynamic_properties.hpp
@@ -27,18 +27,43 @@ namespace shimmer_teqp
 
       const auto T = gerg_mock::T();
       const auto D = gerg_mock::thermodynamic_properties().D;
-      const auto alpha_r = model.alphar(T,
-                                        D,
-                                        mol_frac);
+      // const auto alpha_r = model.alphar(T,
+      //                                   D,
+      //                                   mol_frac);
+
+      const auto Tred = teqp::forceeval(model.red.get_Tr(mol_frac));
+      const auto rhored = teqp::forceeval(model.red.get_rhor(mol_frac));
+      const auto delta = teqp::forceeval(D / rhored);
+      const auto tau = teqp::forceeval(Tred / T);
+      const auto alpha_corr = teqp::forceeval(model.corr.alphar(tau, delta, mol_frac));
+      const auto alpha_dep = teqp::forceeval(model.dep.alphar(tau, delta, mol_frac));
+      const auto alpha_r = alpha_corr + alpha_dep;
       const auto Z = 1 + alpha_r;
       const auto P = D * R * T * Z;
 
-      std::cout.precision(4);
+      auto model_ideal = teqp::GERG2008::GERG2008IdealGasModel(comps);
+      const auto alpha_0 = model_ideal.alphar(T,
+                                              D,
+                                              mol_frac);
+
+      std::cout.precision(16);
       std::cout<< std::scientific<< "D "<< D<< std::endl;
       std::cout<< std::scientific<< "T "<< T<< std::endl;
       std::cout<< std::scientific<< "P "<< P<< std::endl;
       std::cout<< std::scientific<< "Z "<< Z<< std::endl;
       std::cout<< std::scientific<< "a "<< alpha_r<< std::endl;
+
+      ASSERT_DOUBLE_EQ_TOL(gerg_mock::tau(),
+                           tau,
+                           tolerance);
+
+      ASSERT_DOUBLE_EQ_TOL(gerg_mock::delta(),
+                           delta,
+                           tolerance);
+
+      ASSERT_DOUBLE_EQ_TOL(gerg_mock::alpha_0(),
+                           alpha_0,
+                           tolerance);
 
       ASSERT_DOUBLE_EQ_TOL(gerg_mock::alpha_r(),
                            alpha_r,

2dd105d6f5bb8a7f868c13d46431607c24a675f1
Matteo Cicuttin <datafl4sh@toxicnet.eu>
2025-05-14 10:21:26 +0200

    minor.

--- shimmer++/src/solver_main.cpp
+++ shimmer++/src/solver_main.cpp
@@ -19,6 +19,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define SOL_ALL_SAFETIES_ON 1
 #include "sol/sol.hpp"
 
 #include "solver/incidence_matrix.h"
@@ -66,6 +67,32 @@ register_usertypes(sol::state& lua)
     config_ut["tol"] = &config::tol;
 }
 
+static int
+parse_cmdline(int& argc, char**&argv, std::string& luafn, config& cfg)
+{
+    int opt;
+    while ( (opt = getopt(argc, argv, "i:")) != -1) {
+        switch (opt) {
+        case 'i':
+            cfg.database = optarg;
+            break;
+        default:
+            std::cerr << "Error parsing cmdline" << std::endl;
+            return -1;
+            break;
+        }
+    }
+
+    if (cfg.database == "") {
+        std::cerr << "Database not specified (either with ";
+        std::cerr << "-i flag or from config file)";
+        std::cerr << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     if (argc < 2) {
@@ -84,19 +111,20 @@ int main(int argc, char **argv)
     config cfg;
     lua["config"] = &cfg;
 
-    try {
-        lua.safe_script_file(argv[1]);
-    }
-    catch (const sol::error& e) {
-        std::cout << e.what() << std::endl;
+    auto sresult = lua.safe_script_file(argv[1], sol::script_pass_on_error);
+    if (!sresult.valid()) {
+        sol::error err = sresult;
+        std::cerr << "Solver configuration: " << err.what() << std::endl;
         return 1;
     }
+
+    //parse_cmdline(argc, argv, cfg);
 
     shimmer::infrastructure infra;
 
     int err = shimmer::load(cfg.database, infra);
     if (err != SHIMMER_SUCCESS) {
-        std::cout << "Problem detected while loading DB" << std::endl;
+        std::cerr << "Problem detected while loading DB" << std::endl;
         return 1;
     }
 

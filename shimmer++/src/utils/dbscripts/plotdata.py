cc1045323401f39b4636691cbb681a0118598898
karolCascavita <klcascavitam@unal.edu.co>
2025-10-29 16:06:50 +0100

    PythonIO: Add legend and color/marker.

--- shimmer++/src/utils/dbscripts/plotdata.py
+++ shimmer++/src/utils/dbscripts/plotdata.py
@@ -1,14 +1,21 @@
 import shimmer as shmr
 import matplotlib.pyplot as plt
 
-ndfname = "refined_gasco_autogen.db"
+path = ""
+ndfname = path + "refined_gasco_autogen.db"
 
 ndf = shmr.open_ndf(ndfname)
 cur = ndf.cursor()
 
 nodes = [3] + list(range(43,25,-1)) + [1]
 
-for node in nodes:
+num_lines = len(nodes)
+colors  = plt.get_cmap('jet', num_lines) 
+markers = ['+','x','o','*','s','^','v','D','>','<','p','h']
+
+
+for i in range (num_lines):
+    node = nodes[i]
     print(node)
     cur.execute(
         """
@@ -20,8 +27,15 @@ for node in nodes:
     data = cur.fetchall()
     data = list(zip(*data))
 
-    plt.plot(data[1])
+    clr = colors(i % colors.N)
+    mkr = markers[i % len(markers)]
+    
+    plt.plot(data[1], color=clr,  marker=mkr, markevery=20, label="St."+str(node))
 
+plt.title("Molar fractions")
+plt.ylabel("y [-]")        
+plt.xlabel("Time [s]")
+plt.legend()
 plt.show()
 
 shmr.close_ndf(ndf)

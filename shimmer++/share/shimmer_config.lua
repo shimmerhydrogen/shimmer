711712776636ba788e774f9984e60b39e28e00aa
karolCascavita <klcascavitam@unal.edu.co>
2025-10-30 12:32:32 +0100

    Adapt shimmer_config.lua with quality tracking variables.

--- shimmer++/share/shimmer_config.lua
+++ shimmer++/share/shimmer_config.lua
@@ -1,5 +1,6 @@
 config.database = "test_network_1.db"
 config.steps = 7
 config.dt = 3600
-config.do_quality_tracking = false
-
+config.refine = false
+config.quality_tracking = false
+config.qt_steady= false
